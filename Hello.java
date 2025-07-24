1f45620e03cb82dbdfd19470e472ca286721a359
Mohammed Baqtiyar Ahmed Khan <84869024+Baqtiyar@users.noreply.github.com>
2025-07-24 18:50:59 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Hello{
+        public static void main(String[] args){
+                System.out.println("Hello Everyone");
+                Date currentDate = new Date();
+                System.out.println("today's date is: "+ currentDate);
+        }
+}
